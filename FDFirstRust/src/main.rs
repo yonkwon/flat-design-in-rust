4c822ab1131e652940f1fea94454f6cbbedd0705
yonkwon <ykwon90@gmail.com>
2025-01-27 15:15:11 -0600

    Initialize parameters at the start of main function for improved simulation setup

--- FDFirstRust/src/main.rs
+++ FDFirstRust/src/main.rs
@@ -13,17 +13,19 @@ use experiment_manager_alt::ExperimentManager;
 use hdf5_manager_alt::HDF5Manager;
 use rayon::ThreadPoolBuilder;
 
-fn main() -> std::io::Result<()> {
+fn main() -> std::io::Result<()> {    
+    params::initialize_once_cells();
+    println!("Simulation through {:?}", params::PARAM_STRING);
+
     let mut num_thread = params::MAX_THREAD;
     if num_thread > available_parallelism().unwrap().get() {
         num_thread = available_parallelism().unwrap().get()
     }
     ThreadPoolBuilder::new().num_threads(num_thread).build_global().expect("Failed to build the global Rayon thread pool");
     println!("This simulation will run on {} threads", num_thread);
-    
-    params::initialize_once_cells();
-    let mut experiment_manager = ExperimentManager::new();
 
+    let mut experiment_manager = ExperimentManager::new();
+    
     if params::GET_GRAPH {
         let folder_name = (*params::PARAM_STRING).clone();
         if !Path::new(&folder_name).exists() {
